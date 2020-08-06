4417d1746007dbe65187f0fc121e4b6521ea8a77
Nicolas Silva <nicolas.silva@nt.digital>
2020-08-06 20:57:12 -0300

    'init'

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, ImageBackground, Image, Text, StyleSheet } from 'react-native';
+
+const Details = () => {
+    return (
+        <View />
+    );
+};
+
+export default Details;
